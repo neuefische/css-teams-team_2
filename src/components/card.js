12d859b49d42b2abe349175f4be091901855a190
sascharissling <sascha.rissling@gmail.com>
2019-11-08 14:37:24 +0100

    add functionality

---
+++ src/components/card.js
@@ -0,0 +1,17 @@
+export const card = `
+<div class="content__card">
+  <div class="content__card__imgcontainer">
+    <img
+      src=${element.img}
+      alt="trabant car"
+      class="content__card__image"
+    />
+  </div>
+  <div class="content__card__text">
+    <h3>Jaguar Model O</h3>
+    <p>Lorem ipsum dolor sit amet consectetur,</p>
+    <a href="./pages/detail.html">
+      <button class="content__card__button">Read more</button>
+    </a>
+  </div>
+</div>`;
